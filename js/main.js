7f7b4460447e71753b32d03603c2c959f1574655
0todeploy <0todeploy@gmail.com>
2025-06-18 19:48:13 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 // js/main.js
+
+// AOS Initialization
 AOS.init({
   duration: 1000,
   once: true,
 });
+
+// Toggle mobile menu
+function toggleMenu() {
+  const nav = document.getElementById("mainNav");
+  nav.classList.toggle("active");
+}
